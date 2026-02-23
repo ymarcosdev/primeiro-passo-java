2d454e68a3d5b704d0bae3fe0e11a6661d1f6437
ymarcosdev <ymarcosvalente3131@gmail.com>
2026-02-23 17:19:22 -0300

    lógica matemática simples em Java

---
+++ Program3.java
@@ -0,0 +1,19 @@
+
+public class Program3 {
+    public static void main(String[]args) {  
+	int x;
+	double y;
+	x =5;
+    y =2*x;
+    
+    double b,B,h,area;
+    b=6;
+    B=8;
+    h=5;
+    area =(b+B)/2.0*h;
+    
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(area);   
+    }
+}
